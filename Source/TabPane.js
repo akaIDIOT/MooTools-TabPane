06794ecda5317e182ad0dd8db9fe7f203a5d1d87
Mattijs Ugen <mattijsugen@gmail.com>
2012-02-20 15:51:26 +0100

    Reformat entire file to use tabs

--- Source/TabPane.js
+++ Source/TabPane.js
@@ -30,35 +30,35 @@ if (!typeOf) {
 }
 
 var TabPane = this.TabPane = new Class({
-    
-    Implements: [Events, Options],
+	
+	Implements: [Events, Options],
 
-    options: {
-        tabSelector: '.tab',
-        contentSelector: '.content',
-        activeClass: 'active'
-    },
+	options: {
+		tabSelector: '.tab',
+		contentSelector: '.content',
+		activeClass: 'active'
+	},
 
-    container: null,
+	container: null,
 
-    initialize: function(container, options, showNow) {
-        this.setOptions(options);
+	initialize: function(container, options, showNow) {
+		this.setOptions(options);
 
-        this.container = document.id(container);
-        this.container.getElements(this.options.contentSelector).setStyle('display', 'none');
+		this.container = document.id(container);
+		this.container.getElements(this.options.contentSelector).setStyle('display', 'none');
 
-        this.container.addEvent('click:relay(' + this.options.tabSelector + ')', function(event, tab) {
-            this.showTab(this.container.getElements(this.options.tabSelector).indexOf(tab), tab);
-        }.bind(this));
+		this.container.addEvent('click:relay(' + this.options.tabSelector + ')', function(event, tab) {
+			this.showTab(this.container.getElements(this.options.tabSelector).indexOf(tab), tab);
+		}.bind(this));
 
-        if (typeOf(showNow) == 'function') {
-            showNow = showNow();
-        } else {
-            showNow = showNow || 0;
-        }
+		if (typeOf(showNow) == 'function') {
+			showNow = showNow();
+		} else {
+			showNow = showNow || 0;
+		}
 
-        this.showTab(showNow);
-    },
+		this.showTab(showNow);
+	},
 
 	get: function(index) {
 		// TODO: allow index to be a tab header and find the corresponding content (or the other way around)? 
@@ -75,32 +75,32 @@ var TabPane = this.TabPane = new Class({
 		content.inject(this.container.getElements(this.options.contentSelector).getLast(), 'after');
 	},
 
-    showTab: function(index, tab) {
-        var content = this.container.getElements(this.options.contentSelector)[index];
-        if (!tab) {
-            tab = this.container.getElements(this.options.tabSelector)[index];
-        }
+	showTab: function(index, tab) {
+		var content = this.container.getElements(this.options.contentSelector)[index];
+		if (!tab) {
+			tab = this.container.getElements(this.options.tabSelector)[index];
+		}
 
-        if (content) {
-            this.container.getElements(this.options.tabSelector).removeClass(this.options.activeClass);
-            this.container.getElements(this.options.contentSelector).setStyle('display', 'none');
-            tab.addClass(this.options.activeClass);
-            content.setStyle('display', 'block');
-            this.fireEvent('change', index);
-        } 
-    },
+		if (content) {
+			this.container.getElements(this.options.tabSelector).removeClass(this.options.activeClass);
+			this.container.getElements(this.options.contentSelector).setStyle('display', 'none');
+			tab.addClass(this.options.activeClass);
+			content.setStyle('display', 'block');
+			this.fireEvent('change', index);
+		} 
+	},
 
-    closeTab: function(index) {
-        var tabs     = this.container.getElements(this.options.tabSelector);
-        var selected = tabs.indexOf(this.container.getElement('.' + this.options.activeClass)); // is always equals to index 
-        
-        tabs[index].destroy();
-        this.container.getElements(this.options.contentSelector)[index].destroy();
-        this.fireEvent('close', index);
+	closeTab: function(index) {
+		var tabs	 = this.container.getElements(this.options.tabSelector);
+		var selected = tabs.indexOf(this.container.getElement('.' + this.options.activeClass)); // is always equals to index 
+		
+		tabs[index].destroy();
+		this.container.getElements(this.options.contentSelector)[index].destroy();
+		this.fireEvent('close', index);
 
-        // 'intelligently' selecting a tab is sadly not possible, the tab has already been switched before this method is called 
-        this.showTab(index == tabs.length - 1 ? selected - 1 : selected);
-    }
+		// 'intelligently' selecting a tab is sadly not possible, the tab has already been switched before this method is called 
+		this.showTab(index == tabs.length - 1 ? selected - 1 : selected);
+	}
 
 });
 
